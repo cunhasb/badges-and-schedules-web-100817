3c06b4ff9981f2690502b5288ece897b2dca95d5
cunhasb CUNHA <cunhasb@aol.com>
2017-10-03 04:20:38 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,3 +6,10 @@ end
 def batch_badge_creator(attendees)
   attendees.map {|attendee| badge_maker(attendee)}
 end
+
+def assign_rooms(attendees)
+  (0..attendees.size).map do |index|
+     "Hello, #{attendees[index]}! You'll be assigned to room #{index+1}!"}
+   end
+ end
+ 
